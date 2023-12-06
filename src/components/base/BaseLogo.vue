516a04385f6b6cacb96bfb45d2191367e9a89095
Amaljith64 <amaljith64@gmail.com>
2023-12-06 21:33:58 +0530

    12

--- src/components/base/BaseLogo.vue
+++ src/components/base/BaseLogo.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link to="/" class="aside-logo mg-lg-b-20 mg-xl-b-20"  style="text-align: center">
+    <router-link :to="{ name: 'Login' }" class="aside-logo mg-lg-b-20 mg-xl-b-20"  style="text-align: center">
               <img src="../../assets/img/logo.png" style="width: 100%" />
      </router-link>
 </template>
