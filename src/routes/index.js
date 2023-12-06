9317096c17322d2c90b85cc12ddff60f2d2fe249
Amaljith64 <amaljith64@gmail.com>
2023-12-06 21:17:07 +0530

    08

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,31 +2,28 @@ import { createRouter, createWebHistory } from "vue-router";
 import LoginView from '../views/login/LoginView.vue';
 import PasswordResetView from '../views/password/PasswordResetView.vue';
 import AuthLayout from '../layouts/AuthLayout.vue';
+const routes = [
+    {
+        path: "/testhost/",
+        component: AuthLayout,
+        children: [
+            {
+                path: '',
+                name: 'Login',
+                component: LoginView
+            },
+            {
+                path: "request-reset-password",
+                name: 'PasswordReset',
+                component: PasswordResetView,
+            },
+        ]
+    },
+];
+
 const router = createRouter({
     history: createWebHistory(),
-    
-    routes: [
-        {
-            path: "/testhost/",
-            component: AuthLayout,
-            children:[
-                {
-                    path:'',
-                    name:'Login',
-                    component:LoginView
-                },
-                {
-                    path: "request-reset-password",
-                    name:'PasswordReset',
-                    component: PasswordResetView,
-        
-                },
-
-            ]
-
-
-        },
-        
-    ],
+    routes,
 });
-export default router
+
+export default router;
